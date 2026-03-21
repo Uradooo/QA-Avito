d4319b05528ae56beebc65e33838dac8a22a91fc
Vladislav Popov <V11121977@yandex.ru>
2026-03-21 21:04:30 +0400

    minor change

--- tests/accountTests/openLoginForm.test.ts
+++ tests/accountTests/openLoginForm.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import {LoginPopupPage} from "../../pages/loginPopupPage/loginPopupPage";
 import {MainPage} from "../../pages/mainPage/mainPage";
 
-test.describe("Проверки попапа сс авторизацией", () => {
+test.describe("Проверки поп апа с авторизацией", () => {
     test("переход на регистрацию по кнопке", async ({ page }) => {
         //arrange
         const loginPopup = new LoginPopupPage(page);
